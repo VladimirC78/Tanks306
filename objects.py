8178af722d587d78217958c6959faef5251b6b1f
VladimirC78 <chuchalinva@gmail.com>
2023-12-10 13:56:30 +0300

    Исправил название параметра

--- objects.py
+++ objects.py
@@ -1,18 +1,18 @@
 class Tank:
-    def __init__(self, x, y,v,omega, scale,type):
+    def __init__(self, x, y, v, omega, scale, type):
         self.type = type
         self.r = list([x, y])  # Координаты танка по осям х и у
         self.scale = scale  # Характерный размер танка
-        self.v = v # Модуль скорости
-        self.ang = 0 #изначально танк направлен вертикально вверх, угол отсчитывается против часовой стрелки
-        self.omega = omega #потом подберем
+        self.v = v  # Модуль скорости
+        self.ang = 0  # изначально танк направлен вертикально вверх, угол отсчитывается против часовой стрелки
+        self.omega = omega  # потом подберем
         self.charges = 5  # Количество выстрелов у танка, перезаряжается со временем
 
 
 class Bullet:
-    def __init__(self, x, y, v, rad):
+    def __init__(self, x, y, v, scale):
         self.r = list([x, y])  # Координаты танка по осям х и у
-        self.rad = rad  # Радиус пули
+        self.scale = scale  # Радиус пули
         self.v = v  # Двумерный вектор со скоростями по осям
 
 # TODO Возможно, в процессе нужно будет добавить еще какие-то параметры
